9fe711936c571709837ccfb9504c76be5118c892
Daixin Tian <105831455+AlezHibali@users.noreply.github.com>
2023-01-23 14:43:42 -0500

    Update server.c

--- lab1/server.c
+++ lab1/server.c
@@ -9,7 +9,7 @@
 
 int main (int argc, char *argv[]) {
     if (argc != 2){
-        printf("server <UDP listen port>");
+        printf("server <UDP listen port>\n");
         return -1;
     }
 
@@ -26,37 +26,37 @@ int main (int argc, char *argv[]) {
     /* socket initialization */
     socketfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (socketfd < 0){
-        printf("Error with socket file descriptor creation.");
+        printf("Error with socket file descriptor creation.\n");
         return -1;
     }
 
     /* IP and Port number */
     server_info.sin_family = AF_INET;
     server_info.sin_port = htons(port);
-    server_info.sin_addr.s_addr = htonl("128.100.13.139");
+    server_info.sin_addr.s_addr = htonl(INADDR_ANY);
 
     /* socket binding */
     int bind_res = bind(socketfd, (struct sockaddr*)&server_info, sizeof(server_info));
     if (bind_res < 0){
-        printf("Error with bind. Try with different port num.");
+        printf("Error with bind. Try with different port num.\n");
         return -1;
     }
 
     /* Listen and Receive message */
     long recv_size = recvfrom(socketfd, client_msg, sizeof(client_msg), 0, (struct sockaddr*)&client_info, &client_len);
     if (recv_size < 0){
-        printf("Message Receive Error.");
+        printf("Message Receive Error.\n");
         return -1;
     }
 
     /* Respond to Client */
-    bool response = (client_msg == "ftp");
+    bool response = strcmp(client_msg, "ftp");
     if (response) strcpy(server_msg, "yes");
     else strcpy(server_msg, "no");
 
-    long send_size = sendto(socketfd, server_msg, strlen(server_msg), 0, (struct sockaddr*)&client_info, &client_len);
+    long send_size = sendto(socketfd, server_msg, strlen(server_msg), 0, (struct sockaddr*)&client_info, client_len);
     if (send_size < 0){
-        printf("Message Sending Error.");
+        printf("Message Sending Error.\n");
         return -1;
     }
 
